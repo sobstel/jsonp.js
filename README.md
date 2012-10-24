4abaf32b5c771cb7e4d62d472147cae1c767aa79
Przemek Sobstel <przemek@sobstel.org>
2012-10-24 18:32:53 +0200

    Initial import.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 jsonp.js
 ========
 
-Lightweight JSONP library for cross-domain ajax calls (written in pure Javascript).
+JSONP cross-domain ajax calls written in pure Javascript.
+
+Usage
+-----
+
+      $jsonp.send('url?callback=handleStuff', {
+        callbackName: 'handleStuff',
+        onSuccess: function(json){
+          console.log('success!', json);
+        },
+        onTimeout: function(){
+          console.log('timeout!');
+        },
+        timeout: 5
+      });
+
+Notes
+-----
+
+You need to add callback name to URL yourself if you need it, eg.
+
+$jsonp.send('url?callback=name', { callbackName: 'name' });
+
---
+++ jsonp.js
@@ -0,0 +1,32 @@
+/* jsonp.js, (c) Przemek Sobstel 2012, License: MIT */
+
+var $jsonp = (function(){
+  var that = {};
+
+  that.send = function(src, options) {
+    var callback_name = options.callbackName || 'callback',
+      on_success = options.onSuccess || function(){},
+      on_timeout = options.onTimeout || function(){},
+      timeout = options.timeout || 10;
+
+    var timeout_trigger = window.setTimeout(function(){
+      window[callback_name] = function(){};
+      on_timeout();
+    }, timeout * 1000);
+
+    window[callback_name] = function(data){
+      window.clearTimeout(timeout_trigger);
+      on_success(data);
+    };
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = src;
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
+
+  return that;
+})();
+
