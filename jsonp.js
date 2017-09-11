e2932a6a72a478b5bf20a26967885c920f66c085
Tim Beadle <tim@takkconsulting.co.uk>
2017-09-11 10:33:44 +0100

    The module is already called jsonp, so just export the send method

--- jsonp.js
+++ jsonp.js
@@ -19,38 +19,29 @@
   }
 })(this, function(exports) {
 
-  exports.jsonp = (function () {
-    var that = {};
+  exports.send = function(src, options) {
+    var options = options || {},
+    callback_name = options.callbackName || 'callback',
+    on_success = options.onSuccess || function () {},
+    on_timeout = options.onTimeout || function () {},
+    timeout = options.timeout || 10;
 
-    that.send = function(src, options) {
-      var options = options || {},
-      callback_name = options.callbackName || 'callback',
-      on_success = options.onSuccess || function () {},
-      on_timeout = options.onTimeout || function () {},
-      timeout = options.timeout || 10;
+    var timeout_trigger = window.setTimeout(function () {
+      window[callback_name] = function () {};
+      on_timeout();
+    }, timeout * 1000);
 
-      var timeout_trigger = window.setTimeout(function () {
-        window[callback_name] = function () {};
-        on_timeout();
-      }, timeout * 1000);
-
-      window[callback_name] = function (data) {
-        window.clearTimeout(timeout_trigger);
-        on_success(data);
-      };
-
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.async = true;
-      script.src = src;
-
-      document.getElementsByTagName('head')[0].appendChild(script);
+    window[callback_name] = function (data) {
+      window.clearTimeout(timeout_trigger);
+      on_success(data);
     };
 
-    return that;
-  })();
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = src;
 
-  // retain backwards-compatible name
-  exports.$jsonp = exports.jsonp;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
 
 });
